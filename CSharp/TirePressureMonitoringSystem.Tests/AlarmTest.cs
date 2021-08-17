80217e89d6e40000e1a208066d8211a658e25eca
Шаймухаметов Ильдар <ildar.shaymukhametov@gmail.com>
2021-08-17 15:38:01 +0500

    rename test method to Alarm_is_not_initially_on

--- CSharp/TirePressureMonitoringSystem.Tests/AlarmTest.cs
+++ CSharp/TirePressureMonitoringSystem.Tests/AlarmTest.cs
@@ -5,7 +5,7 @@ namespace TDDMicroExercises.TirePressureMonitoringSystem
     public class AlarmTest
     {
         [Fact]
-        public void Foo()
+        public void Alarm_is_not_initially_on()
         {
             Alarm alarm = new Alarm();
             Assert.False(alarm.AlarmOn);
